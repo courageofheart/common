78307f0f2bc1e5c6ee5b4a957245c943ae8e90e0
zhanggf <zhanggf@ynt.ai>
2018-12-29 11:04:31 +0800

    堆函数修改

--- include/common/heap.h
+++ include/common/heap.h
@@ -2,7 +2,18 @@
 #ifndef __HEAP_H_
 #define __HEAP_H_
 
+#include <stdint.h>
+
 /* 堆结构 */
+
+/*
+int (*CompareFunc)(void *left, void *right);：回调函数说明
+
+如果left>right，返回true，表示最大堆
+如果left<right，返回true，表示最小堆
+最大堆还是最小堆可以看heap_shift_down函数的实现
+
+*/
 
 #ifdef __cplusplus
 extern "C"
@@ -14,37 +25,40 @@ extern "C"
 	{
 		void **pstRoot;                 //堆数据数组
 		int size;                       //结构大小
-		int used;                       //已经使用的空间
-	}head_t;
+		int used;                       //已经使用的空间（本质上是下一个节点的下标，类似于stl中的end()方法）
+	}STHeapHead;
 
 	typedef int(*CompareFunc)(void *, void *);
 
 	typedef void(*ReleaseFunc)(void *);
 
+	//初始化
+	STHeapHead * heap_init(uint32_t iSize);
+
 	//重新分配内存空间
-	int heap_reserve(head_t *pstHead, int iSize);
+	int heap_reserve(STHeapHead *pstHead, uint32_t iSize);
 
 	//堆压入一个元素
-	int heap_push(head_t *pstHead, void *pvData, CompareFunc func);
+	int heap_push(STHeapHead *pstHead, void *pvData, CompareFunc func);
 
 	//判断堆是否为空
-	int head_empty(head_t *pstHead);
+	int heap_empty(STHeapHead *pstHead);
 
 	//堆的释放
-	int head_release(head_t *pstHead, ReleaseFunc func);
+	int heap_release(STHeapHead *pstHead, ReleaseFunc func);
 
 	//获取堆顶元素
-	void *heap_top(head_t *pstHead);
+	void *heap_top(STHeapHead *pstHead);
 
 	//弹出堆顶元素
-	void *heap_pop(head_t *pstHead, CompareFunc func);
+	void *heap_pop(STHeapHead *pstHead, CompareFunc func);
 
 	//堆的清空
-	int head_clear(head_t *pstHead);
+	int heap_clear(STHeapHead *pstHead);
 
-	//typedef void(*ShowFunc)(void *);
-
-	//void test_show(head_t *pstHead, ShowFunc func);
+	typedef void(*doFunc)(void *);
+	//堆的遍历
+	void heap_foreach(STHeapHead *pstHead, doFunc cb);
 
 
 #ifdef __cplusplus
--- src/heap.c
+++ src/heap.c
@@ -1,5 +1,4 @@
 
-
 #include <stdlib.h>
 #include <string.h>
 
@@ -8,16 +7,16 @@
 /********************************************************
    Func Name: heap_shift_up
 Date Created: 2018-9-3
- Description: 节点上移
+ Description: 新增节点
 	   Input:   pstHead：头结点
 	              index：插入下标
 		         pvData：数据
 				   func：比较函数
 	  Output:   
       Return: 
-     Caution: 
+     Caution: 新增叶子节点，与根节点比较，调整位置
 *********************************************************/
-static void heap_shift_up(head_t *pstHead, int index, void *pvData, CompareFunc func)
+static void heap_shift_up(STHeapHead *pstHead, int index, void *pvData, CompareFunc func)
 {
 	int sonIndex = index;
 	//节点的索引从0开始，所以是(sonIndex-1)/2;
@@ -27,7 +26,7 @@ static void heap_shift_up(head_t *pstHead, int index, void *pvData, CompareFunc 
 	//sonIndex ---因为需要跟根节点进行比较，所以不能用parentIndex
 	while (sonIndex && func(pvData, pstHead->pstRoot[parentIndex]))
 	{
-		//父节点开始上移，此时空下一个位置
+		//父节点开始下移，此时原父节点处空下一个位置
 		pstHead->pstRoot[sonIndex] = pstHead->pstRoot[parentIndex];
 		sonIndex = parentIndex;
 		parentIndex = (sonIndex - 1) / 2;
@@ -39,23 +38,25 @@ static void heap_shift_up(head_t *pstHead, int index, void *pvData, CompareFunc 
 /********************************************************
    Func Name: heap_shift_down
 Date Created: 2018-9-3
- Description: 节点下移
+ Description: 弹出节点
 	   Input:   pstHead：头结点
 	              index：删除下标
 				   func：比较函数
 	  Output:   
       Return: 
-     Caution: 
+     Caution: 根节点弹出，导致左右叶子节点上移
 *********************************************************/
-static void heap_shift_down(head_t *pstHead, int index, CompareFunc func)
+static void heap_shift_down(STHeapHead *pstHead, int index, CompareFunc func)
 {
 	int parentIndex = index;
 	//右孩子节点下标
 	int sonIndex = (parentIndex + 1) * 2;
 
+	//pstHead->used这个下标处没有任何元素，之所以使用<=是为了确认有没有右孩子节点
 	while (sonIndex <= pstHead->used)
 	{
-		//sonIndex == pstHead->used 判断是否有右孩子
+		//sonIndex == pstHead->used 表示没有右孩子
+		//sonIndex < pstHead->used 表示有右孩子
 		//func(pstHead->pstRoot[sonIndex-1],pstHead->pstRoot[sonIndex]) --- 比较左孩子和右孩子谁的数值大
 		sonIndex -= (sonIndex == pstHead->used) || func(pstHead->pstRoot[sonIndex - 1], pstHead->pstRoot[sonIndex]);
 		//跟父节点数据交换
@@ -66,8 +67,18 @@ static void heap_shift_down(head_t *pstHead, int index, CompareFunc func)
 		sonIndex = (parentIndex + 1) * 2;
 	}
 
-	//parentIndex < pstHead->used - 1 如果 最右最下的节点 已经被处理过了，就不需要再进行处理了
-	//也可以写成 NULL != pstHead->pstRoot[pstHead->used - 1] 说明最后一个节点数据有效
+	/*
+	     parentIndex < pstHead->used - 1这种情况下，表示最深一层的叶子节点不是以完全二叉树的形态存在，
+	假设最深层有4个叶子节点，分别为a,b,c,d(d即为最右最下的叶子节点)，在此场景下，有可能a空余出来，
+	也有可能b空余出来，还有可能c空余出来，
+	绝对不可能是d空余出来，先考虑d空余出来，d实际上不符合parentIndex < pstHead->used - 1，
+	d如果空余出来，应该是parentIndex == pstHead->used - 1
+	如果分别考虑a,b,c谁空余出来，比较复杂，不如将d这个元素拿出来，插入到a,b,c谁空余就插到谁那边，
+	然后调用heap_shift_up()函数重新调整一下顺序即可，这样又是一个完全二叉树啦
+
+	当前"parentIndex < pstHead->used - 1"也可以写成 NULL != pstHead->pstRoot[pstHead->used - 1] 说明最后一个节点数据有效
+
+	*/
 	if (parentIndex < pstHead->used - 1)
 	{
 		//将最右最下的节点插到空缺处
@@ -75,6 +86,40 @@ static void heap_shift_down(head_t *pstHead, int index, CompareFunc func)
 		pstHead->pstRoot[pstHead->used - 1] = NULL;
 	}
 	
+}
+
+/********************************************************
+   Func Name: heap_init
+Date Created: 2018-9-3
+ Description: 初始化堆
+	   Input: iSize：需要分配的内存空间
+	  Output:   
+      Return: 堆头结点
+     Caution: 
+*********************************************************/
+STHeapHead * heap_init(uint32_t iSize)
+{
+	STHeapHead *head = NULL;
+	void **pstTemp = NULL;
+
+	head = malloc(sizeof(STHeapHead));
+	if (NULL == head)
+	{
+		return NULL;
+	}
+	memset(head, 0, sizeof(STHeapHead));
+
+	head->pstRoot = (void **)malloc(iSize * sizeof(void *));
+	if (NULL == head->pstRoot)
+	{
+		return NULL;
+	}
+	memset(head->pstRoot, 0, iSize * sizeof(void *));
+
+	head->size = iSize;
+
+	return head;
+
 }
 
 /********************************************************
@@ -87,7 +132,7 @@ Date Created: 2018-9-3
       Return: error code
      Caution: 
 *********************************************************/
-int heap_reserve(head_t *pstHead,int iSize)
+int heap_reserve(STHeapHead *pstHead, uint32_t iSize)
 {
 	int new_size = 0;
 	void **pstTemp = NULL;
@@ -126,7 +171,7 @@ Date Created: 2018-9-3
       Return: error code
      Caution: 
 *********************************************************/
-int heap_push(head_t *pstHead, void *pvData, CompareFunc func)
+int heap_push(STHeapHead *pstHead, void *pvData, CompareFunc func)
 {
 	if (NULL == pstHead || NULL == func)
 	{
@@ -153,7 +198,7 @@ Date Created: 2018-9-3
       Return: 非0表示空，0表示非空
      Caution: 
 *********************************************************/
-int head_empty(head_t *pstHead)
+int heap_empty(STHeapHead *pstHead)
 {
 	if (NULL == pstHead)
 	{
@@ -173,7 +218,7 @@ Date Created: 2018-9-3
       Return: error code
      Caution: 会释放堆结构中的内存数据
 *********************************************************/
-int head_release(head_t *pstHead, ReleaseFunc func)
+int heap_release(STHeapHead *pstHead, ReleaseFunc func)
 {
 	int i = 0;
 
@@ -211,7 +256,7 @@ Date Created: 2018-9-3
       Return: error code
      Caution: 不会释放堆结构中的内存数据
 *********************************************************/
-int head_clear(head_t *pstHead)
+int heap_clear(STHeapHead *pstHead)
 {
 	if (NULL == pstHead)
 	{
@@ -239,7 +284,7 @@ Date Created: 2018-9-3
       Return: 堆顶元素
      Caution: 
 *********************************************************/
-void *heap_top(head_t *pstHead)
+void *heap_top(STHeapHead *pstHead)
 {
 	void * pvRes = NULL;
 
@@ -265,7 +310,7 @@ Date Created: 2018-9-3
       Return: 堆顶元素
      Caution: 会在堆结构中清除堆顶元素，堆顶元素的内存由外部函数释放
 *********************************************************/
-void *heap_pop(head_t *pstHead, CompareFunc func)
+void *heap_pop(STHeapHead *pstHead, CompareFunc func)
 {
 	void * pvRes = NULL;
 
@@ -284,13 +329,28 @@ void *heap_pop(head_t *pstHead, CompareFunc func)
 	return pvRes;
 }
 
-//void test_show(head_t *pstHead, ShowFunc func)
-//{
-//	int i = 0;
-//	for (;i < pstHead->used; i++)
-//	{
-//		func(pstHead->pstRoot[i]);
-//	}
-//}
+/********************************************************
+   Func Name: heap_foreach
+Date Created: 2018-12-29
+ Description: 堆的遍历
+	   Input:   pstHead：头结点
+	                 cb：回调函数
+	  Output:   
+      Return: 
+     Caution: 回调函数中不可以删除元素或者释放元素
+*********************************************************/
+void heap_foreach(STHeapHead *pstHead, doFunc cb)
+{
+	int i = 0;
 
+	if (NULL == pstHead || NULL == cb)
+	{
+		return;
+	}
+	for (i = 0; i < pstHead->used; i++)
+	{
+		cb(pstHead->pstRoot[i]);
+	}
+
+}
 
