0ec8bd8ebfce4755e25b30e6cd88aec5b9084724
zhanggf <zhanggf@ynt.ai>
2019-01-04 14:28:26 +0800

    更新md5加密算法

--- src/test.c
+++ src/test.c
@@ -10,53 +10,22 @@
 #include "common/regularhelper.h"
 #include "common/base64.h"
 #include "common/gtl_list.h"
+#include "common/crypto_md5.h"
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
 
-
-typedef struct tagSTStudent
-{
-	STGTListNode stNode;
-	int age;
-	int no;
-}STStudent;
-
 void test()
 {
-	int i = 0;
-	time_t t_start, t_end;
-	STGTListHead * head = NULL;
-	STStudent *pstStu = NULL;
+	int result = 0;
+	const char *p = "hello world";
+	char *data = NULL;
 
-	time(&t_start);
+	result = md5_calculate((char *)p, strlen(p), &data);
 
-	head = GTList_Head_Init();
-
-	for (i = 0; i < 1024 * 1024 * 200; i++)
-	{
-		STStudent *pstu = malloc(sizeof(STStudent));
-		pstu->no = i;
-		pstu->age = i / 100;
-		GTList_PushBack(head, &pstu->stNode);
-	}
-
-	while (!GTList__Empty(head))
-	{
-		pstStu = LIST_ENTRY(GTList_PopEnd(head), STStudent, stNode);
-		if (pstStu)
-		{
-			free(pstStu);
-			pstStu = NULL;
-		}
-	}
-	time(&t_end);
-
-	printf("start[%ld]--------end[%ld] . \n", t_start, t_end);
-	printf("ok . \n");
-
+	printf("result = [%d] ,md5 = %s \n",result, data);
 }
 int main(int argc,char * argv[])
 {
